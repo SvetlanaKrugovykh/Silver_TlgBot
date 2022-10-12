694eacf1190e1b1bf91b25ddbb9f858adf3db87a
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2022-10-12 15:20:28 +0300

    change readme and mail env

--- modules/mailer.js
+++ modules/mailer.js
@@ -1,6 +1,8 @@
 const nodemailer = require('nodemailer');
+const SENDER =  process.env.SENDER;
+const MAILHOST =  process.env.MAILHOST;
 
-const recipient = 'svetlana@silver-service.com.ua';
+const recipient = 'zzz@mmm.com';
 const Subject   = 'It`s real mail from tgbot'; 
 const text      = 'dfghfghfgh /n sertsertert /n ertertert'; 
 const html      = "<b>Hello world?</b>";
@@ -8,7 +10,7 @@ const filename  = 'test3.txt';
 const path      = '';
 
 const message = {
-    from: 'support@silver-service.com.ua',
+    from: `${SENDER}`,
     to: recipient,
     subject: Subject,
     text: text,
@@ -22,11 +24,11 @@ attachments: [{
 async function sendmail(message) {
 
     let transporter = nodemailer.createTransport({
-        host: 'nsrv-mx.silver-service.com.ua',
+        host: `${MAILHOST}`,
         port: 25,
         secure: false, 
         auth: {
-            user: 'support@silver-service.com.ua',
+            user: `${SENDER}`,
             pass: undefined
         }
     });
