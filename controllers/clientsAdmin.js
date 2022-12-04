3aa23e583435d945b5cf7595f0fb5c777be138ea
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2022-12-04 11:00:49 +0200

    Added message sender to id

--- controllers/clientsAdmin.js
+++ controllers/clientsAdmin.js
@@ -1,58 +1,67 @@
 const { MArkup, Composer, Scenes } = require('telegraf');
 const axios = require(`axios`);
 const URL = process.env.URL;
-const AUTH_TOKEN =  process.env.AUTH_TOKEN;
+const AUTH_TOKEN = process.env.AUTH_TOKEN;
 
 const startStep = new Composer();
 
-startStep.on( "text", async (ctx) => {
-   try {
-      await ctx.replyWithHTML("Введіть <i>номер телефону </i> або <i>адресу через # </i>, що є в договорі на абонентське обслуговування\n");
-      return ctx.wizard.next();
-   } catch (err) {
-      console.log(err);
-   }
-  });
+startStep.on("text", async (ctx) => {
+	try {
+		await ctx.replyWithHTML("Введіть <i>номер телефону </i> або <i>адресу через # </i>, що є в договорі на абонентське обслуговування\n");
+		return ctx.wizard.next();
+	} catch (err) {
+		console.log(err);
+	}
+});
 
-  const conditionStep = new Composer();
-  conditionStep.on( "text", async (ctx) => {
-     try {
-           let inputLine = ctx.message.text;
-           console.log('inputLine:',inputLine);
-              axios({
-                 method: 'post',
-                 url: URL,
-                 responseType: 'string', 
-                 headers: {
-                    Authorization: `${AUTH_TOKEN}`,
-                    'Content-Type': 'application/json',
-                 },
-                 data: {
-                    Query: `Execute;__GetClientsInfo__;${inputLine};КОНЕЦ`,
-                 }             
-              }) 
-              .then((response) => {
-                 if (!response.status==200) {
-                    ctx.replyWithHTML(`⛔️Ніякої інформації за запитом не знайдено`);
-                    return ctx.scene.leave();
-                 } else {
-                    let answer = response.data.toString();
-                    console.log(answer);
-                    ctx.replyWithHTML(`🥎\n ${answer}.\n`);    
-                 }
-                 })
-              .catch((err) => {
-                 ctx.replyWithHTML(`⛔️Жодної інформації за запитом не знайдено`);
-                 return ctx.scene.leave();
-              })
-              .then(() => {
-                 ctx.replyWithHTML("👋💙💛 Have a nice day!\n");
-              });
-        return ctx.scene.leave();
-     } catch (err) {
-        console.log(err);
-     }
-    });
+const conditionStep = new Composer();
+conditionStep.on("text", async (ctx) => {
+	try {
+		let inputLine = ctx.message.text;
+		if (inputLine.includes("id#")) {
+			let id = inputLine.split("id#")[1];
+			console.log(ctx.message.text);
+			await ctx.replyWithHTML("Введіть текст повідомлення для відправки\n");
+			ctx.wizard.next();
+			console.log(id);
+			await ctx.telegram.sendMessage(id, `Дякуємо за звернення, відповідь: \n ${ctx.message.text}`);
+			return ctx.scene.leave();
+		}
+		console.log('inputLine:', inputLine);
+		axios({
+			method: 'post',
+			url: URL,
+			responseType: 'string',
+			headers: {
+				Authorization: `${AUTH_TOKEN}`,
+				'Content-Type': 'application/json',
+			},
+			data: {
+				Query: `Execute;__GetClientsInfo__;${inputLine};КОНЕЦ`,
+			}
+		})
+			.then((response) => {
+				if (!response.status == 200) {
+					ctx.replyWithHTML(`⛔️Ніякої інформації за запитом не знайдено`);
+					return ctx.scene.leave();
+				} else {
+					let answer = response.data.toString();
+					console.log(answer);
+					ctx.replyWithHTML(`🥎\n ${answer}.\n`);
+				}
+			})
+			.catch((err) => {
+				ctx.replyWithHTML(`⛔️Жодної інформації за запитом не знайдено`);
+				return ctx.scene.leave();
+			})
+			.then(() => {
+				ctx.replyWithHTML("👋💙💛 Have a nice day!\n");
+			});
+		return ctx.scene.leave();
+	} catch (err) {
+		console.log(err);
+	}
+});
 
 const clientsAdminScene = new Scenes.WizardScene('clientsAdminWizard', startStep, conditionStep);
 
